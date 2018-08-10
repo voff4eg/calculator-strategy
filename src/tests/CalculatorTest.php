9f47536e4c71e96bbc6589f3540276b3384f36aa
voff4eg <vladimir.eogor.res@gmail.com>
2018-08-10 08:37:30 +0300

    refactoring

--- src/tests/CalculatorTest.php
+++ src/tests/CalculatorTest.php
@@ -13,12 +13,12 @@ class CalculatorTest extends \PHPUnit\Framework\TestCase
     final public function testPlusTwoNumbers(): void
     {
         $sum = new Calculator('+');
-        $a = random_int(0, 10000);
-        $b = random_int(0, 10000);
+        $firstArgument = random_int(0, 10000);
+        $secondArgument = random_int(0, 10000);
 
         $this->assertEquals(
-            $a + $b,
-            $sum->calculate($a, $b)
+            $firstArgument + $secondArgument,
+            $sum->calculate($firstArgument, $secondArgument)
         );
     }
 
@@ -28,12 +28,12 @@ class CalculatorTest extends \PHPUnit\Framework\TestCase
     final public function testMinusTwoNumbers(): void
     {
         $sum = new Calculator('-');
-        $a = random_int(0, 10000);
-        $b = random_int(0, 10000);
+        $firstArgument = random_int(0, 10000);
+        $secondArgument = random_int(0, 10000);
 
         $this->assertEquals(
-            $a - $b,
-            $sum->calculate($a, $b)
+            $firstArgument - $secondArgument,
+            $sum->calculate($firstArgument, $secondArgument)
         );
     }
 
@@ -43,12 +43,12 @@ class CalculatorTest extends \PHPUnit\Framework\TestCase
     final public function testMultiplyTwoNumbers(): void
     {
         $sum = new Calculator('*');
-        $a = random_int(0, 10000);
-        $b = random_int(0, 10000);
+        $firstArgument = random_int(0, 10000);
+        $secondArgument = random_int(0, 10000);
 
         $this->assertEquals(
-            $a * $b,
-            $sum->calculate($a, $b)
+            $firstArgument * $secondArgument,
+            $sum->calculate($firstArgument, $secondArgument)
         );
     }
 
@@ -58,12 +58,12 @@ class CalculatorTest extends \PHPUnit\Framework\TestCase
     final public function testDivideTwoNumbers(): void
     {
         $sum = new Calculator('/');
-        $a = random_int(0, 10000);
-        $b = random_int(0, 10000);
+        $firstArgument = random_int(0, 10000);
+        $secondArgument = random_int(0, 10000);
 
         $this->assertEquals(
-            floor($a / $b),
-            $sum->calculate($a, $b)
+            floor($firstArgument / $secondArgument),
+            $sum->calculate($firstArgument, $secondArgument)
         );
     }
 
@@ -73,10 +73,10 @@ class CalculatorTest extends \PHPUnit\Framework\TestCase
     final public function testDivideByZero(): void
     {
         $sum = new Calculator('/');
-        $a = random_int(0, 10000);
-        $b = 0;
+        $firstArgument = random_int(0, 10000);
+        $secondArgument = 0;
 
         $this->expectException(RuntimeException::class);
-        $sum->calculate($a, $b);
+        $sum->calculate($firstArgument, $secondArgument);
     }
 }
